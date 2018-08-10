ad3911b39f5c69039051ff3b14aa7ff8b5beb338
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-10 14:30:12 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # person.rb
+class Person
+def initialize(name, job)
+    def name
+      @name = name 
+    end
+    def job
+      @job = job
+    end
+    def name= (new_name)
+      @name = new_name
+    end
+    def job= (new_job)
+      @job = new_job
+    end
+end
+
+new_person = Person()
