9b96e77bd7fcf1e8aed7d62183749caa81211fce
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-10 14:25:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,22 +4,18 @@ class Dog
     @name = name
     @breed = breed
   end
-  
   def name= (new_name)
     @name = new_name
   end
-  
   def breed
     @breed = breed 
   end
-  
   def breed= (new_breed)
     @breed = new_breed 
   end
 end
-
 new_dog = Dog.new("Fido")
 puts new_dog.name
-new_dog = Dog.new("Cocker Spaniel")
+new_dog = Dog.new("Pug")
 puts new_dog.breed
  
