41d26f286094c098a56e6394c37e31cb2bd851d0
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-10 14:19:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  def name= (new_name)
+    @name = new_name
+  end
+  
+  def 
+  def breed
+    @breed = breed 
+  end
+  def breed= 
+    @breed = breed 
+  end
+  
+  sophia = Dog.new("Sophia","Dalmatian")
+  sophia = Dog.name="Cookie"
